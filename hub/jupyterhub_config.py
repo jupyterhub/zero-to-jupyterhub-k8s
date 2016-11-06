848f4837b51849d3fd4977ad4eecff5eb1fcf310
SaladRaider <peterkangveerman@gmail.com>
2016-11-06 11:32:21 -0800

    Testing to see if it works without volumes

--- hub/jupyterhub_config.py
+++ hub/jupyterhub_config.py
@@ -6,7 +6,7 @@ c.JupyterHub.spawner_class = 'kubespawner.KubeSpawner'
 c.JupyterHub.proxy_cmd = '/usr/local/bin/nchp'
 c.JupyterHub.ip = '0.0.0.0'
 
-c.KubeSpawner.namespace = os.environ.get('POD_NAMESPACE', 'default')
+#c.KubeSpawner.namespace = os.environ.get('POD_NAMESPACE', 'default')
 #c.KubeSpawner.hub.api_url = 'https://{host}:{port}'.format(
 #    host=os.environ['KUBERNETES_SERVICE_HOST'],
 #    port=os.environ['KUBERNETES_SERVICE_PORT']
@@ -21,11 +21,12 @@ c.KubeSpawner.start_timeout = 60 * 5  # Upto 5 minutes, first pulls can be reall
 c.KubeSpawner.singleuser_image_spec = 'yuvipanda/simple-singleuser:v1'
 
 # Add volumes to singleuser pods
-c.KubeSpawner.volumes = [{'name': 'volume-{username}-{userid}', 'persistentVolumeClaim': {'claimName': '{username}-{userid}'}}]
+#c.KubeSpawner.volumes = [{'name': 'volume-peter-test', 'persistentVolumeClaim': [{'claimName': 'peter-test-claim'}]}]
+#c.KubeSpawner.volume_mounts = [{'mountPath': '/', 'name': 'volume-peter-test'}]
 
 # The spawned containers need to be able to talk to the hub, ok through the proxy!
 c.KubeSpawner.hub_connect_ip = os.environ['HUB_PROXY_SERVICE_HOST']
-c.KubeSpawner.hub_connect_port = os.environ['HUB_PROXY_SERVICE_PORT']
+c.KubeSpawner.hub_connect_port = int(os.environ['HUB_PROXY_SERVICE_PORT'])
 
 # Do not use any authentication at all
 c.JupyterHub.authenticator_class = 'dummyauthenticator.DummyAuthenticator'
