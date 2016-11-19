a39dcc3223476609c83c755b0ef9770e2307e118
Derrick Mar <derrickmar1215@berkeley.edu>
2016-11-18 21:14:15 -0800

    Fix image pull config misnaming

--- hub/jupyterhub_config.py
+++ hub/jupyterhub_config.py
@@ -23,7 +23,7 @@ c.KubeSpawner.start_timeout = 60 * 15
 
 # Our simplest user image! Optimized to just... start, and be small!
 c.KubeSpawner.singleuser_image_spec = 'data8/jupyterhub-k8s-user:ds8-jhub-from-scratch'
-c.KubeSpawner.image_pull_policy = 'Always'
+c.KubeSpawner.singleuser_image_pull_policy = 'Always'
 
 # Configure dynamically provisioning pvc
 c.KubeSpawner.pvc_name_template = 'claim-{username}-{userid}'
