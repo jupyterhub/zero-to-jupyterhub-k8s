1ea9ece5b7ef4ebfa020045eac884031583b3b19
Derrick Mar <derrickmar1215@berkeley.edu>
2016-11-27 10:58:09 -0800

    Change back to master tag before merging

--- hub/jupyterhub_config.py
+++ hub/jupyterhub_config.py
@@ -22,8 +22,7 @@ c.KubeSpawner.namespace = os.environ.get('POD_NAMESPACE', 'default')
 c.KubeSpawner.start_timeout = 60 * 20
 
 # Our simplest user image! Optimized to just... start, and be small!
-c.KubeSpawner.singleuser_image_spec = 'data8/jupyterhub-k8s-user:ds8-jhub-from-scratch'
-c.KubeSpawner.singleuser_image_pull_policy = 'Always'
+c.KubeSpawner.singleuser_image_spec = 'data8/jupyterhub-k8s-user:master'
 
 # Configure dynamically provisioning pvc
 c.KubeSpawner.pvc_name_template = 'claim-{username}-{userid}'
