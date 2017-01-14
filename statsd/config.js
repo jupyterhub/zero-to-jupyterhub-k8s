5d3dbbd062c3bc9cc514cb70c9dc6e0a8c2a9b1f
YuviPanda <yuvipanda@gmail.com>
2017-01-14 14:23:54 +0530

    Don't set any tags for datadog
    we don't really use it

--- statsd/config.js
+++ statsd/config.js
@@ -3,7 +3,6 @@
         port: parseInt(process.env.STATSD_PORT) || 8125,
         backends: ["statsd-datadog-backend"],
         datadogApiKey: process.env.DATADOG_API_KEY,
-        datadogPrefix: process.env.DATADOG_PREFIX,
-        datadogTags: ["hub:" + process.env.HUB_NAME]
+        datadogPrefix: process.env.DATADOG_PREFIX
     };
 })()
