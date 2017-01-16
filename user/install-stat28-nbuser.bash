846eeaf1e1784c29d9a3fd7289854cb5d47105c8
ryanlovett <rylo@berkeley.edu>
2017-01-16 06:34:41 +0000

    Use NB_USER- and root-specific install scripts.

--- user/install-stat28-nbuser.bash
+++ user/install-stat28-nbuser.bash
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+# Install R and irkernel.
+${CONDA_DIR}/bin/conda install -c r --quiet --yes \
+        r-base=3.3.2 \
+        r-irkernel=0.7.1 \
+        r-rmarkdown=1.2
+
 # nbrsessionproxy notebook extension
 ${CONDA_DIR}/bin/pip install -U --no-deps \
     git+https://github.com/ryanlovett/nbrsessionproxy.git@v0.1.5
 ${CONDA_DIR}/bin/jupyter serverextension enable --sys-prefix --py nbrsessionproxy
 ${CONDA_DIR}/bin/jupyter nbextension install    --sys-prefix --py nbrsessionproxy
 ${CONDA_DIR}/bin/jupyter nbextension enable     --sys-prefix --py nbrsessionproxy
+
+${CONDA_DIR}/bin/conda clean -tipsy
--- user/install-stat28-root.bash
+++ user/install-stat28-root.bash
@@ -2,14 +2,6 @@
 
 set -e
 
-# Install R and irkernel.
-${CONDA_DIR}/bin/conda install -c r --quiet --yes \
-	r-base=3.3.2 \
-	r-irkernel=0.7.1 \
-	r-rmarkdown=1.2
-
-${CONDA_DIR}/bin/conda clean -tipsy
-
 # RStudio Server
 VERSION=1.0.136
 URL="https://download2.rstudio.org/rstudio-server-${VERSION}-amd64.deb"
