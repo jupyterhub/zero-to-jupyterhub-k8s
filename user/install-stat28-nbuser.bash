f22e6f888f0e40890002e41acd9c67db65de3909
ryanlovett <rylo@berkeley.edu>
2017-01-17 05:39:54 +0000

    Install R from MRAN.

--- user/install-stat28-nbuser.bash
+++ user/install-stat28-nbuser.bash
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Install R and irkernel.
-${CONDA_DIR}/bin/conda install -c r --quiet --yes \
-        r-base=3.3.2 \
-        r-irkernel=0.7.1 \
-        r-rmarkdown=1.2
-
 # nbrsessionproxy notebook extension
 ${CONDA_DIR}/bin/pip install -U --no-deps \
     git+https://github.com/ryanlovett/nbrsessionproxy.git@v0.1.6
--- user/install-stat28-root.bash
+++ user/install-stat28-root.bash
@@ -2,6 +2,36 @@
 
 set -e
 
+R_REPO="https://mran.revolutionanalytics.com/snapshot/2017-01-16"
+MRAN_KEY="E084DAB9"
+GPG_KEY_SERVER="keyserver.ubuntu.com"
+U_CODE="jessie-cran3"
+
+# Configure apt repository for R packages
+echo "deb ${R_REPO}/bin/linux/debian ${U_CODE}/" > \
+    /etc/apt/sources.list.d/mran.list
+gpg --keyserver ${GPG_KEY_SERVER} --recv-key ${MRAN_KEY}
+gpg -a --export ${MRAN_KEY} | apt-key add -
+echo -n | openssl s_client -connect mran.revolutionanalytics.com:443 | \
+    sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' | \
+    tee '/usr/local/share/ca-certificates/mran.revolutionanalytics.com.crt'
+update-ca-certificates
+
+# Install R
+apt-get -q update > /dev/null
+apt-get -y --quiet --no-install-recommends install \
+	r-base r-recommended libopenblas-base
+
+## Define our default R repository
+# Use HTTPS for RProfile to prevent an error message in RStudio.
+R_REPO_HTTPS=${R_REPO//http:/https:}
+echo "options(repos = list(CRAN = '${R_REPO_HTTPS}'))" >> /etc/R/Rprofile.site
+
+# Install R and irkernel
+Rscript -e "install.packages(c('repr', 'IRdisplay', 'evaluate', 'crayon', 'pbdZMQ', 'devtools', 'uuid', 'digest', 'rmarkdown'), repos='${R_REPO}')"
+Rscript -e "devtools::install_github('IRkernel/IRkernel')"
+Rscript -e "IRkernel::installspec(FALSE)"
+
 # RStudio Server
 VERSION=1.0.136
 URL="https://download2.rstudio.org/rstudio-server-${VERSION}-amd64.deb"
@@ -16,7 +46,3 @@ gdebi --non-interactive ${PACKAGE}
 rm ${PACKAGE}
 
 apt-get clean
-
-# RStudio needs to find libR.so
-echo /opt/conda/lib/R/lib > /etc/ld.so.conf.d/stat28-conda.conf
-ldconfig
