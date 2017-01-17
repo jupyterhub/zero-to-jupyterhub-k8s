5e54fb5309ec9e7382f64b8132b4e21139fff61b
ryanlovett <rylo@berkeley.edu>
2017-01-17 05:02:10 +0000

    Upgrade nbrsessionproxy to v0.1.6.

--- user/install-stat28-nbuser.bash
+++ user/install-stat28-nbuser.bash
@@ -8,7 +8,7 @@ ${CONDA_DIR}/bin/conda install -c r --quiet --yes \
 
 # nbrsessionproxy notebook extension
 ${CONDA_DIR}/bin/pip install -U --no-deps \
-    git+https://github.com/ryanlovett/nbrsessionproxy.git@v0.1.5
+    git+https://github.com/ryanlovett/nbrsessionproxy.git@v0.1.6
 ${CONDA_DIR}/bin/jupyter serverextension enable --sys-prefix --py nbrsessionproxy
 ${CONDA_DIR}/bin/jupyter nbextension install    --sys-prefix --py nbrsessionproxy
 ${CONDA_DIR}/bin/jupyter nbextension enable     --sys-prefix --py nbrsessionproxy
