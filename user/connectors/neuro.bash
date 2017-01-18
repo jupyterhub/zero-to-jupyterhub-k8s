d4451221d902fe54cec9791a3992c2c65c80e4ee
ryanlovett <rylo@berkeley.edu>
2017-01-18 04:35:02 +0000

    Update neuro deps. Disable pycortex for now.
    pycortex is not installable at the moment.

--- user/connectors/neuro.bash
+++ user/connectors/neuro.bash
@@ -1,11 +1,12 @@
 #!/bin/bash
 # conda & pip packages for neuro connector (mark.lescroart)
 ${CONDA_DIR}/bin/conda install --quiet --yes \
-            lxml=3.6.4
-
-${CONDA_DIR}/bin/pip install --egg git+https://github.com/gallantlab/pycortex@data8
+	lxml==3.7.2 \
+	nibabel==2.1.0 \
+	tqdm==4.11.0
 
 ${CONDA_DIR}/bin/pip --no-cache-dir install \
-            mne==0.13.0 \
-            nibabel==2.0.2 \
-            tqdm==4.8.4
+	mne==0.13.1
+
+# pycortex can't be installed at the moment
+# ${CONDA_DIR}/bin/pip install git+https://github.com/gallantlab/pycortex@data8
