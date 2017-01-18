cd0fd46017570d585a435dae142221ed03ca88f0
SaladRaider <peterkangveerman@gmail.com>
2017-01-17 16:03:52 -0800

    Upgrade okpy to 1.8.2

--- user/install-python-packages.bash
+++ user/install-python-packages.bash
@@ -40,7 +40,7 @@ ${CONDA_DIR}/bin/conda remove --quiet --yes --force qt pyqt
 # DS8 specific packages
 
 ${CONDA_DIR}/bin/pip --no-cache-dir install \
-            okpy==1.6.4 \
+            okpy==1.8.2 \
             pypandoc==1.2.0 \
             datascience==0.8.2
 
