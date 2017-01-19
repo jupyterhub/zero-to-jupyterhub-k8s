4820a5a2c6810927178d7fa9291c9051a20dd969
Ryan Lovett <rylo@berkeley.edu>
2017-01-19 00:54:26 +0000

    Install cortex library.

--- user/connectors/neuro.bash
+++ user/connectors/neuro.bash
@@ -8,5 +8,11 @@ ${CONDA_DIR}/bin/conda install --quiet --yes \
 ${CONDA_DIR}/bin/pip --no-cache-dir install \
 	mne==0.13.1
 
-# pycortex can't be installed at the moment
+# pycortex can't be installed from the repository at the moment
 # ${CONDA_DIR}/bin/pip install git+https://github.com/gallantlab/pycortex@data8
+
+# Installs the cortex library
+git clone https://github.com/gallantlab/pycortex
+cd pycortex
+git checkout data8
+${CONDA_DIR}/bin/python setup.py install
