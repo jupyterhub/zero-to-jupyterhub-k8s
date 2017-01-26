ffefcb8b9d333d3de376d7568589d378a9db7a92
Ryan Lovett <rylo@berkeley.edu>
2017-01-26 03:05:26 +0000

    Update okpy to 1.9.5.

--- user/install-ds8.bash
+++ user/install-ds8.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 # DS8 specific packages
 ${CONDA_DIR}/bin/pip --no-cache-dir install \
-            okpy==1.9.4 \
+            okpy==1.9.5 \
             pypandoc==1.2.0 \
             datascience==0.8.2
