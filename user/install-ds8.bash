68e492269648afefe8a350fe50561910bec48ee1
Peter Veerman <peterkangveerman@gmail.com>
2017-02-06 06:45:13 +0000

    Upgrade datascience python package to 0.9.1

--- user/install-ds8.bash
+++ user/install-ds8.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 # DS8 specific packages
 ${CONDA_DIR}/bin/pip --no-cache-dir install \
-            datascience==0.8.2 \
+            datascience==0.9.1 \
             nbgrader==0.4.0rc1 \
             okpy==1.9.5 \
             pypandoc==1.2.0 \
