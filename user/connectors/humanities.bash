b1d6c1e8f6fdff3443b215a8dfe4635020966313
Gunjan Baid <gunjan_baid@berkeley.edu>
2017-02-07 19:32:38 +0000

    Added new comment line in humanities.bash, testing.

--- user/connectors/humanities.bash
+++ user/connectors/humanities.bash
@@ -4,7 +4,8 @@ ${CONDA_DIR}/bin/conda install --quiet --yes \
             gensim=0.12.4 \
             nltk=3.2.1
 
-# We put NLTK data under /opt/conda, and then set the NLTK_DATA
+# We put NLTK data under /opt/conda, 
+# and then set the NLTK_DATA
 # environment variable appropriately. See
 # http://www.nltk.org/data.html#command-line-installation
 # for more info.
