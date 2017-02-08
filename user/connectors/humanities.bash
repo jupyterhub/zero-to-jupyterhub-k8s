f9652f44cabd5ef5959e0ccbca4d54ee60931237
Ryan Lovett <rylo@berkeley.edu>
2017-02-08 22:41:10 +0000

    Correct comments.

--- user/connectors/humanities.bash
+++ user/connectors/humanities.bash
@@ -1,6 +1,5 @@
 #!/bin/bash
-# Conda packages & nltk downloading for humanities connector (troland)
-# and history (lknelson3)
+# Conda packages & nltk downloading for humanities connector
 ${CONDA_DIR}/bin/conda install --quiet --yes \
             gensim=0.12.4 \
             nltk=3.2.1
@@ -12,14 +11,14 @@ ${CONDA_DIR}/bin/conda install --quiet --yes \
 # for more info.
 mkdir ${CONDA_DIR}/nltk
 
-# MODEL_____________________  COURSES___________
-# averaged_perceptron_tagger: history humanities
-#                    cmudict:         humanities
-#          maxent_ne_chunker: history humanities
-#                      punkt: history humanities
-#                  stopwords: history
-#                    wordnet: history humanities
-#                      words: history humanities
+# MODEL_____________________  TERMS____
+# averaged_perceptron_tagger: 17sp 16fa
+#                    cmudict:      16fa
+#          maxent_ne_chunker: 17sp 16fa
+#                      punkt: 17sp 16fa
+#                  stopwords: 17sp
+#                    wordnet: 17sp 16fa
+#                      words: 17sp 16fa
 ${CONDA_DIR}/bin/python -m nltk.downloader -d ${CONDA_DIR}/nltk \
     averaged_perceptron_tagger \
     cmudict \
