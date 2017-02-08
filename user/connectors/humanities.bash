4d4807934b8430fdaff43a43de9766c9015c4dc0
Ryan Lovett <rylo@berkeley.edu>
2017-02-08 22:37:08 +0000

    Install stopwords model. Document course usage.

--- user/connectors/humanities.bash
+++ user/connectors/humanities.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Conda packages & nltk downloading for humanities connector (troland)
+# and history (lknelson3)
 ${CONDA_DIR}/bin/conda install --quiet --yes \
             gensim=0.12.4 \
             nltk=3.2.1
@@ -11,10 +12,20 @@ ${CONDA_DIR}/bin/conda install --quiet --yes \
 # for more info.
 mkdir ${CONDA_DIR}/nltk
 
+# MODEL_____________________  COURSES___________
+# averaged_perceptron_tagger: history humanities
+#                    cmudict:         humanities
+#          maxent_ne_chunker: history humanities
+#                      punkt: history humanities
+#                  stopwords: history
+#                    wordnet: history humanities
+#                      words: history humanities
 ${CONDA_DIR}/bin/python -m nltk.downloader -d ${CONDA_DIR}/nltk \
     averaged_perceptron_tagger \
     cmudict \
     maxent_ne_chunker \
     punkt \
+    stopwords \
     wordnet \
-    words
+    words \
+    ;
