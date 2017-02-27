ef8cebd8741aafb052fbbe3fb177147f01ce66c3
YuviPanda <yuvipanda@gmail.com>
2017-02-26 16:17:55 -0800

    Update pypandoc version to latest
    1.2.0 was no longer installing

--- user/install-ds8.bash
+++ user/install-ds8.bash
@@ -4,6 +4,6 @@ ${CONDA_DIR}/bin/pip --no-cache-dir install \
             datascience==0.9.5 \
             nbgrader==0.4.0rc1 \
             okpy==1.9.5 \
-            pypandoc==1.2.0 \
+            pypandoc==1.3.3 \
             git+https://github.com/choldgraf/connectortools.git@86ea3e3 \
             ;
