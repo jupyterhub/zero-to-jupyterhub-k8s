7ce84303bbae9fd6561d61799c786c95f55c6b11
Ryan Lovett <rylo@berkeley.edu>
2017-03-09 23:03:02 +0000

    Change upstream source of connectortools.

--- user/install-ds8.bash
+++ user/install-ds8.bash
@@ -5,5 +5,5 @@ ${CONDA_DIR}/bin/pip --no-cache-dir install \
             nbgrader==0.4.0rc1 \
             okpy==1.9.5 \
             pypandoc==1.3.3 \
-            git+https://github.com/choldgraf/connectortools.git@86ea3e3 \
+            git+https://github.com/data-8/connector-instructors.git@e7bd553 \
             ;
