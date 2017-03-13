ffa44dc99ce0fd74436031e4d79faff9bb6d6f7b
Gunjan Baid <gunjan_baid@berkeley.edu>
2017-03-13 07:01:27 +0000

    Installing some geog88 modules with conda instead of pip.

--- user/connectors/geog88.bash
+++ user/connectors/geog88.bash
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 ${CONDA_DIR}/bin/pip --no-cache-dir install \
-    geopandas==0.2.1 \
     fiona==1.7.4 \
     mplleaflet==0.0.5 \
-    rtree==0.8.3 \
     ;
 
 ${CONDA_DIR}/bin/conda install --quiet --yes \
-    libspatialindex=1.8.5
+    libspatialindex=1.8.5 \
+    rtree=0.8.3 \
+    geopandas=0.2.1
