52320e9730feb84d91d7a7a89c5df63e893ee2db
Tony Yang <tony@ycyang.me>
2017-04-05 15:25:49 -0700

    fix minor bugs

--- scale/scale.py
+++ scale/scale.py
@@ -77,7 +77,7 @@ def scale(options):
 
     scale_logger.info("Total nodes in the cluster: %i", len(k8s.nodes))
     scale_logger.info(
-        "%i nodes are unschedulable at this time", k8s.get_num_schedulable())
+        "%i nodes are unschedulable at this time", k8s.get_num_unschedulable())
     scale_logger.info("Found %i critical nodes",
                       len(k8s.nodes) - len(nodes))
     scale_logger.info("Recommending total %i nodes for service", goal)
