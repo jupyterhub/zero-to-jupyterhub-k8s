0866c7958a812dcf151e8285b2b7e7930ddeb8d3
Carol Willing <carolcode@willingconsulting.com>
2017-04-26 09:56:14 -0700

    Toggle off ribbon for GitHub fork

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -99,7 +99,7 @@ html_theme_options = {
     'show_powered_by': False,
     'github_user': 'jupyterhub',
     'github_repo': 'zero-to-jupyterhub-k8s',
-    'github_banner': True,
+    'github_banner': False,
     'show_related': False
 }
 
