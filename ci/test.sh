65dd0e425a6f078eb711ec3992ffc1ad25515b68
yuvipanda <yuvipanda@gmail.com>
2017-12-22 15:11:58 -0800

    Add --wait to helm install
    This should make helm wait until the hub and proxy are in
    Running state, and the service has been provisioned.

--- ci/test.sh
+++ ci/test.sh
@@ -6,7 +6,7 @@ IP=$(ifconfig eth0 | grep 'inet addr' | cut -d: -f2 | awk '{print $1}')
 TEST_NAMESPACE=jupyterhub-test
 TEST_URL=http://$IP:31212
 
-helm install --name jupyterhub-test --namespace $TEST_NAMESPACE ./jupyterhub/ -f minikube-config.yaml
+helm install --wait --name jupyterhub-test --namespace $TEST_NAMESPACE ./jupyterhub/ -f minikube-config.yaml
 
 kubectl --namespace=$TEST_NAMESPACE rollout status --watch deployment/hub
 kubectl --namespace=$TEST_NAMESPACE rollout status --watch deployment/proxy
