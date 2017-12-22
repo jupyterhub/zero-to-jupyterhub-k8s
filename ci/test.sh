c57f241e126c7414e367e182b27fe687ba343f24
yuvipanda <yuvipanda@gmail.com>
2017-12-22 15:53:14 -0800

    Provide more debug info when waiting for pods to start
    Also don't wait for pod phases to change, just hit the endpoint
    until we get something useful back. This lets us print out
    proper debug info

--- ci/test.sh
+++ ci/test.sh
@@ -6,14 +6,12 @@ IP=$(ifconfig eth0 | grep 'inet addr' | cut -d: -f2 | awk '{print $1}')
 TEST_NAMESPACE=jupyterhub-test
 TEST_URL=http://$IP:31212
 
-helm install --wait --name jupyterhub-test --namespace $TEST_NAMESPACE ./jupyterhub/ -f minikube-config.yaml
-
-kubectl --namespace=$TEST_NAMESPACE rollout status --watch deployment/hub
-kubectl --namespace=$TEST_NAMESPACE rollout status --watch deployment/proxy
+helm install --name jupyterhub-test --namespace $TEST_NAMESPACE ./jupyterhub/ -f minikube-config.yaml
 
 echo "waiting for servers to become responsive"
 until curl --fail -s $TEST_URL/hub/api; do
-    sleep 5
+    kubectl --namespace=$TEST_NAMESPACE describe pod
+    sleep 10
 done
 
 echo "getting jupyterhub version"
