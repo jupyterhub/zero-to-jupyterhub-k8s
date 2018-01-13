330c4b5ee0a8568258af2124f9a16a2275a77e8d
yuvipanda <yuvipanda@gmail.com>
2018-01-12 23:22:02 -0800

    Add lots of documentation to the shell scripts
    bash scripts need a very high comment to code ratio to even
    have a chance of being maintainable long term.

--- images/image-puller/pull.sh
+++ images/image-puller/pull.sh
@@ -1,15 +1,40 @@
 #!/bin/sh
+# Script to talk to a docker daemon & pull a given image
+#
+# Takes all input in form of environment variables
+#
+# IMAGE_NAME
+# Name of image to be pulling, without tag
+# IMAGE_TAG
+# Tag of the image to be pulling
+#
+# Script expects a docker daemon unix socket with support for at least v1.23
+# of the docker API in /var/run/docker.sock
+#
+# If IMAGE_NAME starts with 'gcr.io', the script will assume it is running on Google Cloud
+# and it has access to the API metadata service to fetch credentials. Then it will fetch
+# credentials and pull from that repository.
+# In the future, this could be extended to other private image registries too.
+# However, it means if you have a gcr.io *public* image and want to pull it anywhere not
+# on Google Cloud, pulling will fail.
+#
+# Requires curl & jq to work
 set -e
+
+# Stores auth info if needed
 AUTH=""
 
-case $IMAGE in gcr.io*)
-                   # gcr.io is being used, assume we are on GKE & authenticate with the metadata service for pulling
-                   PWD=$(curl -s -f -m 10 "http://metadata/computeMetadata/v1/instance/service-accounts/default/token" -H "Metadata-Flavor: Google" \
-                             | jq -r .access_token)
+case ${IMAGE_NAME} in gcr.io*)
+    # Assume we are on google cloud, use metadata service to fetch pulling credentials
+    PASSWORD=$(curl -s -f -m 10 "http://metadata/computeMetadata/v1/instance/service-accounts/default/token" -H "Metadata-Flavor: Google" \
+                | jq -r .access_token)
 
-                   AUTH=$(echo -n "{\"username\": \"_token\", \"email\": \"notval@goo.com\", \"password\": \"${PWD}\"}" \
-                              | base64 \
-                              | tr -d '\n')
+    # This is the format for making the authentication token for docker registry from a username + password
+    AUTH=$(echo -n "{\"username\": \"_token\", \"email\": \"notval@goo.com\", \"password\": \"${PASSWORD}\"}" \
+                | base64 \
+                | tr -d '\n')
 esac
 
-curl -H "X-Registry-Auth: ${AUTH}" -X POST --unix-socket /var/run/docker.sock "http:/v1.23/images/create?fromImage=${IMAGE}&tag=${TAG}"
+# We expect at least v1.23 of the docker API to be present, which is fairly old by now.
+# Since these do not get actively removed, we should be fine for a while
+curl -H "X-Registry-Auth: ${AUTH}" -X POST --unix-socket /var/run/docker.sock "http:/v1.23/images/create?fromImage=${IMAGE_NAME}&tag=${IMAGE_TAG}"
--- images/pre-puller/block.sh
+++ images/pre-puller/block.sh
@@ -1,8 +1,27 @@
 #!/bin/sh
+# Script that does the following actions:
+#  1. Create an image-puller daemonset to fetch the user image on all nodes
+#  2. Check if all nodes have the user image present in them in a loop
+#  3. When the images are present, kill the image-puller daemonset and exit
+#
+# All inputs are passed in as environment variables
+#  1. DAEMONSET_SPEC
+#     A single line JSON of the image-puller daemonset to create
+#  2. KUBERNETES_SERVICE_HOST, KUBERNETES_SERVICE_PORT
+#     The hostname and port to use for talking to the k8s API.
+#     When running in cluster, this is automatically set by Kubernetes
+#  3. IMAGE
+#     Full name of user image
+#
+# jq & curl are required to run this script. Complex jq scripts are kept as separate files.
+#
+# This script is designed to be run from inside a kubernetes cluster only.
+# It will fail if any of the operations fail.
 set -e
 
+# Create a daemonset and capture the output from the k8s API
+# When successfully created, the API output is a JSON object representing the complete spec
 echo "Creating Daemonset..."
-# Set up
 DAEMONSET=$(curl \
     -H "Content-Type: application/json"  \
     -H "Authorization: Bearer $(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" \
@@ -15,9 +34,13 @@ DAEMONSET=$(curl \
     -d "${DAEMONSET_SPEC}"
 )
 
+# Find the generated name of the daemonset from the API response
 DAEMONSET_NAME=$(echo ${DAEMONSET} | jq -r .metadata.name)
 
 pulling_complete() {
+    # Return 0 if all nodes have ${IMAGE} present in them, 1 otherwise
+
+    # Grab definitions of all nodes in the cluster
     NODES=$(curl \
                 -H "Content-Type: application/json" \
                 -H "Authorization: Bearer $(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" \
@@ -29,7 +52,10 @@ pulling_complete() {
                 https://${KUBERNETES_SERVICE_HOST}:${KUBERNETES_SERVICE_PORT}/api/v1/nodes
          );
 
+    # Find out how many nodes we have in total
     TOTAL_NODES=$(echo ${NODES} | jq -r '.items | length')
+
+    # Find out how many nodes report having the image we care about
     COMPLETE_NODES=$(echo ${NODES} | jq -f nodes_with_image.jq --arg image ${IMAGE}| jq -r length)
 
     echo "${COMPLETE_NODES} of ${TOTAL_NODES} complete"
@@ -40,11 +66,14 @@ pulling_complete() {
     fi
 }
 
+# Loop until all nodes have the image we want
 echo "Waiting for all nodes to pull images..."
 while ! pulling_complete; do
     sleep 2;
 done
 
+# Delete the daemonset after pulling is complete
+# We set propagationPolicy to "Foreground" to have the call wait until all the pods from daemonset disappear
 echo "Deleting daemonset"
 curl \
     -H "Content-Type: application/json"  \
@@ -54,7 +83,6 @@ curl \
     --fail \
     --silent \
     --show-error \
-    --verbose \
     https://${KUBERNETES_SERVICE_HOST}:${KUBERNETES_SERVICE_PORT}/apis/extensions/v1beta1/namespaces/${NAMESPACE}/daemonsets/${DAEMONSET_NAME} \
     -d '{"apiVersion": "v1", "kind": "DeleteOptions", "propagationPolicy": "Foreground"}'
 
