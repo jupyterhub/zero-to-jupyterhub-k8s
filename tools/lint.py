4ce4cc6a82721cc4e67cdf00d53d7e44a8f0527f
Erik Sundell <erik.i.sundell@gmail.com>
2018-07-10 16:16:16 +0200

    Updated tools/lint.py to support K8s <=1.11
    `kubeval` requires schemas for the K8s API. These were not available in 
    the default kubeval repo, so while waiting for my PR to be accepted I 
    point towards my repo as a source for the K8s API schemas used by 
    `kubeval` at https://github.com/consideRatio/kubernetes-json-schema

--- tools/lint.py
+++ tools/lint.py
@@ -8,43 +8,54 @@ function, you must install yamllint and kubeval.
 """
 
 
+import os
+import sys
 import argparse
 import glob
 import subprocess
 
+os.chdir(os.path.dirname(sys.argv[0]))
+
 def lint(config, values, kubernetes_version):
-    """Calls `helm lint`, `helm template` and `yamllint`."""
+    """Calls `helm lint`, `helm template`, `yamllint` and `kubeval`."""
 
     output = 'lint-output'
 
-    print("### helm lint")
+    print("### Clearing output directory")
+    subprocess.check_call([
+        'rm', '-rf', output + '/*',
+    ])
+
+    print("### Linting started")
+    print("### 1/4 - helm lint")
     subprocess.check_call([
         'helm', 'lint', '../jupyterhub',
         '--values', values,
     ])
 
-    print("### helm template")
+    print("### 2/4 - helm template")
     subprocess.check_call([
         'helm', 'template', '../jupyterhub',
         '--values', values, 
         '--output-dir', output
     ])
     
-    print("### yamllint")
+    print("### 3/4 - yamllint")
     subprocess.check_call([
         'yamllint', '-c', config, output
     ])
 
-    print("### kubeval")
+    print("### 4/4 - kubeval")
     for filename in glob.iglob(output + '/**/*.yaml', recursive=True):
         subprocess.check_call([
             'kubeval', filename,
             '--kubernetes-version', kubernetes_version,
-            '--strict'
+            '--strict',
+            '--schema-location', 'https://raw.githubusercontent.com/consideRatio'
         ])
 
     print()
-    print("### All good!")
+    print("### Linting finished: All good!")
 
 
 if __name__ == '__main__':
@@ -52,7 +63,7 @@ if __name__ == '__main__':
     argparser.add_argument('--config', default='lint-config.yaml', help='Specify the yamllint config')
     argparser.add_argument('--values', default='lint-chart-values.yaml', help='Specify additional chart value files')
     argparser.add_argument('--output', default='lint-output', help='Specify an output directory')
-    argparser.add_argument('--kubernetes-version', default='1.8.0', help='Validate against this kubernetes version')
+    argparser.add_argument('--kubernetes-version', default='1.10.4', help='Validate against this kubernetes version')
     args = argparser.parse_args()
     
     lint(args.config, args.values, args.kubernetes_version)
