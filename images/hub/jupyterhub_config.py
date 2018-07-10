9ecbac34ff7d5b62c20eca15dd6f23a302378f43
Erik Sundell <erik.i.sundell@gmail.com>
2018-07-10 16:25:16 +0200

    Complementary - Allow setting of storage labels
    Forgot to add these previously...

--- images/hub/jupyterhub_config.py
+++ images/hub/jupyterhub_config.py
@@ -295,12 +295,8 @@ if get_config('cull.enabled', False):
     if get_config('cull.users'):
         cull_cmd.append('--cull-users')
 
-    # FIXME: remove version check when we require jupyterhub 0.9 in the chart
-    # that will also mean we can remove the podCuller image
-    import jupyterhub
-    from distutils.version import LooseVersion as V
     cull_max_age = get_config('cull.max-age')
-    if cull_max_age and V(jupyterhub.__version__) >= V('0.9'):
+    if cull_max_age:
         cull_cmd.append('--max-age=%s' % cull_max_age)
 
     c.JupyterHub.services.append({
