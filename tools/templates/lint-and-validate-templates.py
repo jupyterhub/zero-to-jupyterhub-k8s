6a6d19ae6b614926a0811168b88e969e25e13d8a
Erik Sundell <erik.i.sundell@gmail.com>
2018-07-25 06:12:53 +0200

    Refactored lint.py

--- tools/templates/lint-and-validate-templates.py
+++ tools/templates/lint-and-validate-templates.py
@@ -16,40 +16,38 @@ import subprocess
 
 os.chdir(os.path.dirname(sys.argv[0]))
 
-def lint(config, values, kubernetes_version):
+def lint(yamllint_config, chart_values, kubernetes_version, output_dir):
     """Calls `helm lint`, `helm template`, `yamllint` and `kubeval`."""
-
-    output = 'lint-output'
 
     print("### Clearing output directory")
     subprocess.check_call([
-        'mkdir', '-p', output,
+        'mkdir', '-p', output_dir,
     ])
     subprocess.check_call([
-        'rm', '-rf', output + '/*',
+        'rm', '-rf', output_dir + '/*',
     ])
 
     print("### Linting started")
     print("### 1/4 - helm lint")
     subprocess.check_call([
-        'helm', 'lint', '../jupyterhub',
-        '--values', values,
+        'helm', 'lint', '../../jupyterhub',
+        '--values', chart_values,
     ])
 
     print("### 2/4 - helm template")
     subprocess.check_call([
-        'helm', 'template', '../jupyterhub',
-        '--values', values, 
-        '--output-dir', output
+        'helm', 'template', '../../jupyterhub',
+        '--values', chart_values, 
+        '--output-dir', output_dir
     ])
     
     print("### 3/4 - yamllint")
     subprocess.check_call([
-        'yamllint', '-c', config, output
+        'yamllint', '-c', yamllint_config, output_dir
     ])
 
     print("### 4/4 - kubeval")
-    for filename in glob.iglob(output + '/**/*.yaml', recursive=True):
+    for filename in glob.iglob(output_dir + '/**/*.yaml', recursive=True):
         subprocess.check_call([
             'kubeval', filename,
             '--kubernetes-version', kubernetes_version,
@@ -58,15 +56,15 @@ def lint(config, values, kubernetes_version):
         ])
 
     print()
-    print("### Linting finished: All good!")
+    print("### Linting and validation of templates finished: All good!")
 
 
 if __name__ == '__main__':
     argparser = argparse.ArgumentParser()
-    argparser.add_argument('--config', default='lint-config.yaml', help='Specify the yamllint config')
-    argparser.add_argument('--values', default='lint-chart-values.yaml', help='Specify additional chart value files')
-    argparser.add_argument('--output', default='lint-output', help='Specify an output directory')
-    argparser.add_argument('--kubernetes-version', default='1.10.4', help='Validate against this kubernetes version')
+    argparser.add_argument('--yamllint-config', default='yamllint-config.yaml', help='Specify the yamllint config')
+    argparser.add_argument('--chart-values', default='chart-values.yaml', help='Specify additional chart value files')
+    argparser.add_argument('--kubernetes-version', default='1.10.5', help='Validate against this kubernetes version')
+    argparser.add_argument('--output-dir', default='rendered-templates', help='Specify an output directory for the rendered templates')
     args = argparser.parse_args()
     
-    lint(args.config, args.values, args.kubernetes_version)
+    lint(args.yamllint_config, args.chart_values, args.kubernetes_version, args.output_dir)
