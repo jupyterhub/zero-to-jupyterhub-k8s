51c7a58ac9acafeba1b0353a9457355280b015db
Chris Holdgraf <choldgraf@gmail.com>
2018-08-10 16:09:38 -0700

    Updating sphinx_copybutton import
    Going to self-merge this since the docs will be broken until it's updated!

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -34,7 +34,7 @@ from recommonmark.transform import AutoStructify
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.mathjax',
-              'sphinx_copybutton']
+              'sphinx_copybutton.sphinx_copybutton']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
