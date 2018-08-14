aa948c57f920016add47ad3c222369bb15ebb60d
Erik Sundell <erik.i.sundell@gmail.com>
2018-08-15 00:30:28 +0200

    Initial steps towards smoother Local/Remote CI

--- ci/install.sh
+++ ci/install.sh
@@ -1,30 +1,64 @@
 #!/bin/bash
-set -ex
+# NOTE: The script should be executed from the repo directory
 
+# SCRIPT SUMMARY
+# --------------
+# 1. Downloads binaries into /bin if they don't already exist
+#    - nsenter
+#    - minikube $KUBE_VERSION
+#    - kubectl  $MINIKUBE_VERSION
+#    - helm     $HELM_VERSION
+#    - kubeval  $KUBEVAL_VERSION
+# 2. (LOCAL DEVELOPMENT ONLY) Setup kubectl and minikube autocomplete
+
+# TODO: setup kubectl's and helm's HOME directories within the repo to avoid
+#       influencing installations.
+
+# Set shell options
+# -e : Exit immediately if a command exits with a non-zero status.
+# -u : Treat unset variables as an error when substituting.
+# -x : Print commands and their arguments as they are executed.
+set -eux
+
+# Download binaries
+# -----------------
+# Ensure the bin folder exist
 mkdir -p bin
 
-# install nsenter if missing (needed by kube on trusty)
+# Ensure nsenter exist as needed by kube on Ubuntu 14.04 (trusty)
 if ! which nsenter; then
-  curl -L https://github.com/minrk/git-crypt-bin/releases/download/trusty/nsenter > nsenter
+  echo "installing nsenter"
+  curl -sSLo nsenter https://github.com/minrk/git-crypt-bin/releases/download/trusty/nsenter
   echo "5652bda3fbea6078896705130286b491b6b1885d7b13bda1dfc9bdfb08b49a2e  nsenter" | shasum -a 256 -c -
   chmod +x nsenter
   sudo mv nsenter /usr/local/bin/
 fi
 
-# install kubectl, minikube
-# based on https://blog.travis-ci.com/2017-10-26-running-kubernetes-on-travis-ci-with-minikube
-echo "installing kubectl"
-curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/v${KUBE_VERSION}/bin/linux/amd64/kubectl
-chmod +x kubectl
-mv kubectl bin/
+if ! [ -f bin/kubectl-${KUBE_VERSION} ]; then
+  echo "installing kubectl"
+  curl -sSLo bin/kubectl-${KUBE_VERSION} https://storage.googleapis.com/kubernetes-release/release/v${KUBE_VERSION}/bin/linux/amd64/kubectl
+  chmod +x bin/kubectl-${KUBE_VERSION}
+fi
+cp bin/kubectl-${KUBE_VERSION} bin/kubectl
 
-echo "installing minikube"
-curl -Lo minikube https://storage.googleapis.com/minikube/releases/v${MINIKUBE_VERSION}/minikube-linux-amd64
-chmod +x minikube
-mv minikube bin/
+if ! [ -f bin/helm-${HELM_VERSION} ]; then
+  echo "installing helm"
+  curl -sSLo bin/helm-${HELM_VERSION}.tar.gz https://storage.googleapis.com/kubernetes-helm/helm-v${HELM_VERSION}-linux-amd64.tar.gz
+  tar --extract --file bin/helm-${HELM_VERSION}.tar.gz --directory bin --strip-components 1 linux-amd64/helm
+  rm bin/helm-${HELM_VERSION}.tar.gz
+  mv bin/helm bin/helm-${HELM_VERSION}
+fi
+cp bin/helm-${HELM_VERSION} bin/helm
 
-echo "installing kubeval"
+if ! [ -f bin/minikube-${MINIKUBE_VERSION} ]; then
+  echo "installing minikube"
+  curl -sSLo bin/minikube-${MINIKUBE_VERSION} https://storage.googleapis.com/minikube/releases/v${MINIKUBE_VERSION}/minikube-linux-amd64
+  chmod +x bin/minikube-${MINIKUBE_VERSION}
+fi
+cp bin/minikube-${MINIKUBE_VERSION} bin/minikube
+
 if ! [ -f bin/kubeval-${KUBEVAL_VERSION} ]; then
+  echo "installing kubeval"
   curl -sSLo bin/kubeval-${KUBEVAL_VERSION}.tar.gz https://github.com/garethr/kubeval/releases/download/${KUBEVAL_VERSION}/kubeval-linux-amd64.tar.gz
   tar --extract --file bin/kubeval-${KUBEVAL_VERSION}.tar.gz --directory bin
   rm bin/kubeval-${KUBEVAL_VERSION}.tar.gz
@@ -32,31 +66,8 @@ if ! [ -f bin/kubeval-${KUBEVAL_VERSION} ]; then
 fi
 cp bin/kubeval-${KUBEVAL_VERSION} bin/kubeval
 
-echo "starting minikube with RBAC"
-sudo CHANGE_MINIKUBE_NONE_USER=true $PWD/bin/minikube start --vm-driver=none --kubernetes-version=v${KUBE_VERSION} --extra-config=apiserver.Authorization.Mode=RBAC --bootstrapper=localkube
-minikube update-context
-
-echo "waiting for kubernetes"
-JSONPATH='{range .items[*]}{@.metadata.name}:{range @.status.conditions[*]}{@.type}={@.status};{end}{end}'
-until kubectl get nodes -o jsonpath="$JSONPATH" 2>&1 | grep -q "Ready=True"; do
-  sleep 1
-done
-kubectl get nodes
-
-echo "installing helm"
-curl -ssL https://storage.googleapis.com/kubernetes-helm/helm-v${HELM_VERSION}-linux-amd64.tar.gz \
-  | tar -xz -C bin --strip-components 1 linux-amd64/helm
-chmod +x bin/helm
-
-kubectl --namespace kube-system create sa tiller
-kubectl create clusterrolebinding tiller --clusterrole cluster-admin --serviceaccount=kube-system:tiller
-helm init --service-account tiller
-
-
-echo "waiting for tiller"
-kubectl --namespace=kube-system rollout status --watch deployment/tiller-deploy
-
-echo "installing git-crypt"
-curl -L https://github.com/minrk/git-crypt-bin/releases/download/0.5.0/git-crypt > bin/git-crypt
-echo "46c288cc849c23a28239de3386c6050e5c7d7acd50b1d0248d86e6efff09c61b  bin/git-crypt" | shasum -a 256 -c -
-chmod +x bin/git-crypt
+if [[ ! -z "${CICD}" ]]; then
+  echo "setting up kubectl and minikube autocomplete"
+  source <(kubectl completion bash)
+  source <(minikube completion bash)
+fi
