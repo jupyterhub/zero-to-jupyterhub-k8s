66de55980dec98015a39687f2b07382678c405a8
Erik Sundell <erik.i.sundell@gmail.com>
2019-01-27 19:35:42 +0100

    Fix CD

--- ci/travis-script.sh
+++ ci/travis-script.sh
@@ -5,7 +5,7 @@ python3 tools/templates/lint-and-validate.py
 # render & publish chart
 if [[
     "$TRAVIS_BRANCH" == "master" &&
-    "$KUBE_VERSION" == "1.10.0" &&
+    "$KUBE_VERSION" == "1.13.2" &&
     "$TRAVIS_PULL_REQUEST" == "false"
 ]]; then
     ./ci/deploy.sh
