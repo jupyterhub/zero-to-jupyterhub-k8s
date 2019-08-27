358e69b5af1c588bc1c1c87acc25473d764f906d
Chia-liang Kao <clkao@clkao.org>
2019-08-27 23:36:58 +0800

    fix travis-run-kind

--- ci/vagrant-run-kind.sh
+++ ci/vagrant-run-kind.sh
@@ -2,7 +2,8 @@
 # Run this inside vagrant to test the travis scripts
 
 set -eux
-export SCENARIO=1.14
+export SCENARIO=1.15-default
+export RUNNER=kind
 export TRAVIS_BRANCH=master
 export TRAVIS_PULL_REQUEST=true
 export TRAVIS_COMMIT_RANGE=`git rev-parse --short origin/master`..`git rev-parse --short HEAD`
