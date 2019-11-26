2a183b5c715f1d5030ed8af8ace4664aa6258dbf
Changli Han <changli.han@zalando.de>
2019-11-26 09:48:39 +0100

    add namespace

--- jupyterhub/validate.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-import jsonschema
-import yaml
-
-# HACK: These files are never closed, but is ok!
-schema = yaml.safe_load(open('schema.yaml'))
-values = yaml.safe_load(open('values.yaml'))
-
-jsonschema.validate(values, schema)
