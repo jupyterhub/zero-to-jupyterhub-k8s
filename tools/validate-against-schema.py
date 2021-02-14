765037911220db1813c189406eee230d8d1154af
Erik Sundell <erik.i.sundell@gmail.com>
2021-02-14 20:10:26 +0100

    schema-validation-script: refactor for tools folder

--- tools/validate-against-schema.py
+++ tools/validate-against-schema.py
@@ -1,21 +1,26 @@
 #!/usr/bin/env python3
 import jsonschema
+import os
+import sys
 import yaml
 
-with open("schema.yaml") as f:
+# Change current directory to this directory
+os.chdir(os.path.dirname(sys.argv[0]))
+
+with open("../jupyterhub/schema.yaml") as f:
     schema = yaml.safe_load(f)
+with open("../jupyterhub/values.yaml") as f:
+    values = yaml.safe_load(f)
+with open("templates/lint-and-validate-values.yaml") as f:
+    lint_and_validate_values = yaml.safe_load(f)
 
 # Validate values.yaml against schema
 print("Validating values.yaml against schema.yaml...")
-with open("values.yaml") as f:
-    values = yaml.safe_load(f)
 jsonschema.validate(values, schema)
 print("OK!")
 print()
 
 # Validate lint-and-validate-values.yaml against schema
 print("Validating lint-and-validate-values.yaml against schema.yaml...")
-with open("../tools/templates/lint-and-validate-values.yaml") as f:
-    lint_and_validate_values = yaml.safe_load(f)
 jsonschema.validate(lint_and_validate_values, schema)
 print("OK!")
